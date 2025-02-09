88f68ad8c41682128c9ce95b75cddfc1fcafb66c
Difx <gimarkngar@gmail.com>
2025-02-09 13:10:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function yes() {
+    const pic = document.getElementById('pic');
+    const title = document.getElementById('title');
+    const buttons_hide = document.getElementById('options');
+    const read = document.getElementById('read');
+    options2.style.display = 'none'; 
+    pic.src = 'bugcat-capoo.gif';  
+    title.innerHTML = 'Thx i love youu <3';
+    buttons_hide.style.display = 'none'; 
+    read.style.display = 'block';  
+}
+
+function no() {
+    const pic = document.getElementById('pic');
+    const buttons_hide = document.getElementById('options');
+    const options2 = document.getElementById('options2');  
+    title.innerHTML = 'Do you love me???';
+    pic.src = 'why.gif';
+    buttons_hide.style.display = 'none';  
+    options2.style.display = 'block';  
+}
+
+function hate(){
+    const read2 = document.getElementById('read2')
+    pic.src = 'khóc.gif';
+    title.innerHTML = 'Ok so you hate me !';
+    options2.style.display = 'none'; 
+    read2.style.display ='block';
+}
+
+function fuck() {
+    alert('fu**k you i hate you to 」(￣▽￣」)')
+}
+
+function letter(){
+    const letter = document.getElementById('letter')
+    pic.style.display ='none';
+    read2.style.display ='none';
+    title.style.display ='none';
+    read.style.display ='none';
+    letter.style.display ='block'
+}
